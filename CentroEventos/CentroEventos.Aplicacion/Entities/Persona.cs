07ec1594ef5268c198226373f9ae57989c0f90be
EIK001 <facuvillca@hotmail.com>
2025-05-12 18:18:47 -0300

    echodown

--- CentroEventos/CentroEventos.Aplicacion/Entities/Persona.cs
+++ CentroEventos/CentroEventos.Aplicacion/Entities/Persona.cs
@@ -2,13 +2,14 @@ namespace CentroEventos.Aplicacion;
 // Persona: Id, dni, nombre, apellido, teléfono, correo electrónico.
 public class Persona {
     public int Id { get; set; }
-    public string Dni {get;set;}
+    public string? Dni {get;set;}
     public string? Nombre {get;set;}
     public string? Apellido {get;set;}
     public int Telefono {get;set;}
     public string? Email {get;set;}
 
-    public Persona (int unId, String unNro, string? unNom, string? unAp,int unTel, string? unEm) {
+    public Persona (int unId, String unNro, string? unNom, string? unAp,int unTel, string? unEm) 
+    {
         Id = unId;
         Dni = unNro;
         Nombre = unNom;
@@ -17,7 +18,8 @@ public class Persona {
         Email = unEm;
     }
 
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"ID: {Id},dni: {Dni}, nombre: {Nombre}, apellido: {Apellido}, {Environment.NewLine} telefono: {Telefono}, correo electronico: {Email} ";
     }
 }
