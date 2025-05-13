b12840a7988644a51247649014fb735b3e524b4e
Leandro Bie <107581384+leandrobie@users.noreply.github.com>
2025-05-12 23:02:48 -0300

    lean

--- CentroEventos/CentroEventos.Aplicacion/UseCases/Evento/EventoBajaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCases/Evento/EventoBajaUseCase.cs
@@ -1,6 +1,12 @@
-﻿namespace CentroEventos.Aplicacion.UseCases.Actividad;
+﻿using CentroEventos.Aplicacion.validators;
+namespace CentroEventos.Aplicacion;
 
-public class BajaActividadUseCase(IRepositorioEventoDeportivo repo)
+public class BajaActividadUseCase(IRepositorioEventoDeportivo repo,IRepositorioReserva repoReservas, EventoBajaValidador validador)
 {
-    
+    public void Ejecutar(EventoDeportivo evento)
+    {
+        if (!validador.ValidarEventoBaja(evento,repoReservas,out string msg))
+            throw new Exception(msg);
+        repo.EventoBaja(evento);
+    }
 }
