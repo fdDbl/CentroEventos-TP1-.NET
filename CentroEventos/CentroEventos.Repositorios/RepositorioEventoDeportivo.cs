dbec0ab52381c3857c8e59f56856326ef79e9698
Leandro Bie <107581384+leandrobie@users.noreply.github.com>
2025-05-13 13:40:28 -0300

    lean

--- CentroEventos/CentroEventos.Repositorios/RepositorioEventoDeportivo.cs
+++ CentroEventos/CentroEventos.Repositorios/RepositorioEventoDeportivo.cs
@@ -19,12 +19,51 @@ public class RepositorioEventoDeportivo: IRepositorioEventoDeportivo
 
     public void BajaActividad(int id)
     {
-        throw new NotImplementedException();
+        List<EventoDeportivo> listaEventos= ListarEventos();
+        EventoDeportivo? evento=new EventoDeportivo();
+        evento=listaEventos.Find(EventoDeportivo=> EventoDeportivo.Id==id);
+        if (evento!=null)
+        {
+            listaEventos.Remove(evento);
+            SobreEscribirArchivo(listaEventos);
+        }
+        else
+            throw new EntidadNotFoundException("No existe un evento con ese id.\n"); //Falta crear la clase EntidadNotFoundException
+                                                                                    //Esto iría en el useCase?
+    }
+
+    private void SobreEscribirArchivo(List<EventoDeportivo> listaEventos)
+    {
+        using StreamWriter sw= new StreamWriter(nomArch,false); //false para sobreescribir el archivo
+            foreach (EventoDeportivo e in listaEventos)
+            {
+                sw.WriteLine(e.Id);
+                sw.WriteLine(e.Nombre);
+                sw.WriteLine(e.Descripcion);
+                sw.WriteLine(e.FechaHoraInicio);
+                sw.WriteLine(e.DuracionHoras);
+                sw.WriteLine(e.CupoMaximo);
+                sw.WriteLine(e.ResponsableId);
+            }
     }
 
     public List<EventoDeportivo> ListarActividades()
     {
-        throw new NotImplementedException();
+        using StreamReader sr= new StreamReader(nomArch);
+        List<EventoDeportivo> listaEventos= new List<EventoDeportivo>(); //creo la lista
+        while (!sr.EndOfStream) //mientras no sea fin de archivo
+        {
+            var Evento=new EventoDeportivo(); //Asigno cada campo correspondiente
+            Evento.Id=int.Parse(sr.ReadLine()?? "");
+            Evento.Nombre=sr.ReadLine()?? "";
+            Evento.Descripcion=sr.ReadLine() ?? "";
+            Evento.FechaHoraInicio=DateTime.Parse(sr.ReadLine()?? "");
+            Evento.DuracionHoras=double.Parse(sr.ReadLine()?? "");
+            Evento.CupoMaximo=int.Parse(sr.ReadLine()?? "");
+            Evento.ResponsableId=int.Parse(sr.ReadLine()?? "");
+            listaEventos.Add(Evento); //Agrego el evento a la lista
+        }
+        return listaEventos; //Devuelvo la lista
     }
 
     public void ModificarActividad(EventoDeportivo actividad)
@@ -35,7 +74,7 @@ public class RepositorioEventoDeportivo: IRepositorioEventoDeportivo
         actividad.CupoMaximo = int.Parse(Console.ReadLine());
         Console.WriteLine("Ingrese duracion para modificarla:");
         actividad.DuracionHoras = int.Parse(Console.ReadLine());
-        using StreamReader sr = new StreamReader (nomArch) 
+        using StreamReader sr = new StreamReader (nomArch) ;
         while (!sr.EndOfStream) {
             if (actividad.Nombre == sr.ReadLine()) {
                 sr.Read();
