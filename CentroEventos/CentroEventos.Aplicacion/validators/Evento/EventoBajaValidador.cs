8057f05c0322eb276ff3db789f4c486a58677939
Leandro Bie <107581384+leandrobie@users.noreply.github.com>
2025-05-13 12:58:24 -0300

    lean

--- CentroEventos/CentroEventos.Aplicacion/validators/Evento/EventoBajaValidador.cs
+++ CentroEventos/CentroEventos.Aplicacion/validators/Evento/EventoBajaValidador.cs
@@ -4,5 +4,13 @@ namespace CentroEventos.Aplicacion.validators;
 
 public class EventoBajaValidador
 {
-
+    public bool ValidarEventoBaja (EventoDeportivo actividad,IRepositorioReserva repoReserva, out string msg)
+    {
+        msg="";
+        if (repoReserva.ListarReservas()!=null) //Si la lista != null, quiere decir que tiene reservas asociadas
+        {
+            msg+= "No puede darse de baja el evento debido a que tiene reservas asociadas.\n";
+        }
+        return msg=="";
+    }
 }
