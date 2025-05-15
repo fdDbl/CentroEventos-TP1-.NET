b0508385a07b522350829de85c9981df619cb60e
Federico Dobal <dobalfede@gmail.com>
2025-05-15 15:06:40 -0300

    .

--- CentroEventos/CentroEventos.Aplicacion/UseCases/Persona/BajaPersonaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCases/Persona/BajaPersonaUseCase.cs
@@ -3,6 +3,7 @@
 public class BajaPersonaUseCase(IRepositorioPersona repo)
 {
     public void Ejecutar (int id){
+        // falta chequear que la persona exista. Lo valida un validador, no el repo
         repo.BajaPersona(id);
     }
 }
--- CentroEventos/CentroEventos.Aplicacion/UseCases/Persona/ModificarPersonaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCases/Persona/ModificarPersonaUseCase.cs
@@ -1,8 +1,8 @@
-﻿// namespace CentroEventos.Aplicacion;
-//
-// public class ModificarPersonaUseCase(IRepositorioPersona repo)
-// {
-//     public void Ejecutar (Persona persona ){
-//         repo.ModificarPersona(persona);
-//     }
-// }
+﻿namespace CentroEventos.Aplicacion;
+
+public class ModificarPersonaUseCase(IRepositorioPersona repo)
+{
+    public void Ejecutar (Persona persona ){
+        repo.ModificarPersona(persona); // falta parámetro (no hace falta que se devuelva la persona modificada por parámetro)
+    }
+}
