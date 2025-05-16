28b1c25e318268b68e84e24ac8998a3957c09258
Federico Dobal <dobalfede@gmail.com>
2025-05-16 20:55:05 -0300

    f

--- CentroEventos/CentroEventos.Consola/Program.cs
+++ CentroEventos/CentroEventos.Consola/Program.cs
@@ -1,29 +1,34 @@
 ﻿using CentroEventos.Aplicacion;
 using CentroEventos.Repositorios;
 
-
+// Servicio de autorización
 var servicioAutorizacion = new ServicioAutorizacionProvisorio();
 
+// Validadores de Reserva
 var validadorReservaAlta1 = new ReservaValidador_AltaExistencias();
 var validadorReservaAlta2 = new ReservaValidador_AltaDuplicado();
 var validadorReservaAlta3 = new ReservaAlta_CupoDisponible();
 var validadorReservaBaja = new ReservaValidador_BajaExistencia();
 var validadorReservaMod = new ReservaValidador_ModificarExistentes();
 
+// Repositorios para inyectar
 IRepositorioPersona repositorioPersona = new RepositorioPersona();
 IRepositorioReserva repositorioReserva = new RepositorioReserva();
 IRepositorioEventoDeportivo repositorioEventoDeportivo = new RepositorioEventoDeportivo();
 
+// Casos de uso de EventoDeportivo
 var altaEventoDeportivo = new EventoAltaUseCase(servicioAutorizacion,repositorioEventoDeportivo,repositorioPersona,new EventoAltaValidador());
 var bajaEventoDeportivo = new EventoBajaUseCase(servicioAutorizacion,repositorioEventoDeportivo,repositorioReserva,new EventoBajaValidador());
 var modificarEventoDeportivo = new EventoModificacionUseCase(servicioAutorizacion,repositorioEventoDeportivo,new EventoModificadorValidador());
 var listarEventosDeportivos = new EventoListarUseCase(repositorioEventoDeportivo);
 
+// Casos de uso de Reserva
 var altaReserva = new ReservaAltaUseCase(servicioAutorizacion,repositorioReserva,repositorioEventoDeportivo,repositorioPersona,validadorReservaAlta1,validadorReservaAlta2,validadorReservaAlta3);
 var bajaReserva = new ReservaBajaUseCase(servicioAutorizacion,repositorioReserva,validadorReservaBaja);
 var modificarReserva = new ReservaModificarUseCase(servicioAutorizacion,repositorioReserva,repositorioEventoDeportivo,repositorioPersona,validadorReservaMod);
 var listarReservas = new ReservaListarUseCase(repositorioReserva);
 
+// Programa principal
 try
 {
     var listaReservas = listarReservas.Ejecutar();
