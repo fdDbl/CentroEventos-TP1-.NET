c31a1b71d7e77fca5cb067ce326b4ad317c6757a
isa-cast <isabellacasta2011@outlook.com>
2025-05-15 23:39:24 -0300

    isa

--- CentroEventos/CentroEventos.Aplicacion/validators/Evento/EventoBajaValidador.cs
+++ CentroEventos/CentroEventos.Aplicacion/validators/Evento/EventoBajaValidador.cs
@@ -4,10 +4,10 @@ public class EventoBajaValidador
     public bool ValidarEventoBaja (EventoDeportivo actividad,IRepositorioEventoDeportivo repoEvento,IRepositorioReserva repoReserva, out string msg)
     {
         msg="";
-        // if (repoReserva.ListarReservas()!=null) //Si la lista != null, quiere decir que tiene reservas asociadas
-        // {
-        //     msg+= "No puede darse de baja el evento debido a que tiene reservas asociadas.\n";
-        // }
+            if (repoReserva.ContarReserva(actividad.Id) > 0) //Si la lista != 0, quiere decir que tiene reservas asociadas
+            {
+               msg+= "No puede darse de baja el evento debido a que tiene reservas asociadas.\n";
+            }
         if (repoEvento.ObtenerEvento(actividad.Id)==null) //Verifico si hay un evento con el id recibido como parámetro
         {
             msg+="No existe un evento con el id recibido.\n";
