5d303393490673e00db6dd182a70ecd6bd4e7120
Federico Dobal <dobalfede@gmail.com>
2025-05-16 17:44:18 -0300

    fede

---
+++ CentroEventos/CentroEventos.Aplicacion/Validators/Reserva/Alta/ReservaAltaDuplicado.cs
@@ -0,0 +1,19 @@
+﻿namespace CentroEventos.Aplicacion;
+
+public class ReservaAltaDuplicado
+{
+    public bool Validar(Reserva reserva, IRepositorioReserva repoReserva, out string msg)
+    {
+        msg = "";
+        
+        var lista = repoReserva.ListarReservas();
+        Reserva? rCheck;
+        int pId = reserva.PersonaId;
+        int eId = reserva.EventoDeportivoId;
+        rCheck = lista.Find(r => r.PersonaId == pId && r.EventoDeportivoId == eId);
+        if (rCheck != null)
+            msg += "La persona no puede reservar dos veces el mismo Evento Deportivo.\n";
+
+        return msg == "";
+    }
+}
--- CentroEventos/CentroEventos.Aplicacion/Validators/Reserva/Alta/ReservaAltaValidador.cs
+++ CentroEventos/CentroEventos.Aplicacion/Validators/Reserva/Alta/ReservaAltaValidador.cs
@@ -9,13 +9,7 @@ public class ReservaAltaValidador
         if(repoPersona.ObtenerPersona(reserva.PersonaId) == null || repoEvento.ObtenerEvento(reserva.EventoDeportivoId) == null)
             msg += "Persona y/o Evento Deportivo no existente(s).\n";
         
-        var lista = repoReserva.ListarReservas();
-        Reserva? rCheck;
-        int pId = reserva.PersonaId;
-        int eId = reserva.EventoDeportivoId;
-        rCheck = lista.Find(r => r.PersonaId == pId && r.EventoDeportivoId == eId);
-        if(rCheck != null)
-            msg += "La persona no puede reservar dos veces el mismo Evento Deportivo.\n";
+
         
         if (repoReserva.ContarReserva(reserva.EventoDeportivoId) == repoEvento.ObtenerEvento(reserva.EventoDeportivoId)?.CupoMaximo)
             msg += "No hay cupo disponible para el evento que se desea reservar.\n";
