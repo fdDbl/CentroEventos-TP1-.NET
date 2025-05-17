9f05cb4e95f338b70932969a23b1f9ee2d058115
Leandro Bie <107581384+leandrobie@users.noreply.github.com>
2025-05-17 15:07:28 -0300

    lean- EventoBaja validadores y useCase

---
+++ CentroEventos/CentroEventos.Aplicacion/Validators/Evento/Baja/EventoBaja_ValidadorReservasAsociadas.cs
@@ -0,0 +1,13 @@
+namespace CentroEventos.Aplicacion;
+public class EventoBaja_ValidadorReservasAsociadas, IRepositorioReserva repoReservas, out string msg)
+{
+    public bool Validar(EventoDeportivo evento, IRepositorioReserva repoReservas, out string msg)
+    {
+        msg="";
+        if (repoReservas.ContarReserva(evento.Id)>0)
+        {
+            msg+= "No puede darse de baja el evento debido a que tiene reservas asociadas.\n";
+        }
+        return msg=="";
+    }
+}
