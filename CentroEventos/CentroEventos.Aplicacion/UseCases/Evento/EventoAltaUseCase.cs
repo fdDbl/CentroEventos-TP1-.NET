98b9faae64aabe3d1f46ebf341438a8673b4cc26
isa-cast <isabellacasta2011@outlook.com>
2025-05-16 23:52:45 -0300

    isa

--- CentroEventos/CentroEventos.Aplicacion/UseCases/Evento/EventoAltaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCases/Evento/EventoAltaUseCase.cs
@@ -1,14 +1,25 @@
 ﻿namespace CentroEventos.Aplicacion;
 
-public class EventoAltaUseCase(IServicioAutorizacion auth,IRepositorioEventoDeportivo repoAct, IRepositorioPersona repoPer, EventoAltaValidador validador)
+public class EventoAltaUseCase(IServicioAutorizacion auth,IRepositorioEventoDeportivo repoAct, IRepositorioPersona repoPer, EventoAltaValidadorNombre vNom, EventoAltaValidadorCupoMaximo vCupo)
 {
     public void Ejecutar(EventoDeportivo evento, int userId)
     {
-        if (auth.PoseeElPermiso(userId, Permiso.EventoAlta))
+        if (!auth.PoseeElPermiso(userId, Permiso.EventoAlta))
         {
-            if (!validador.ValidarEventoAlta(evento, repoPer, out string msg)) // valido
-                throw new Exception(msg);
-            repoAct.EventoAlta(evento); // si es valido doy de alta el evento
+            throw new Exception("No posee los permisos para realizar operacion");
         }
+
+        if (!vNom.ValidarEventoAltaNombre(evento, repoPer, out string msg1))
+        { // valido
+            throw new ValidacionException(msg1);
+        }
+
+        if (!vCupo.ValidarEventoAltaCupoMaximo(evento, repoPer, out string msg2))
+        { // valido
+            throw new ValidacionException(msg2);
+        }
+ // ... seguir , faltan un par
+        repoAct.EventoAlta(evento); // si es valido doy de alta el evento
+        
     }
 }
