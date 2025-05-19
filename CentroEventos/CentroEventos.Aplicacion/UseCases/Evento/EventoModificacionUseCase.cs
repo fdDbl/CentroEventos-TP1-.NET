f5b34d1bbdaa8c6ea9b4b79e8ac486c9f7e183ab
isa-cast <isabellacasta2011@outlook.com>
2025-05-18 21:26:52 -0300

    isa

--- CentroEventos/CentroEventos.Aplicacion/UseCases/Evento/EventoModificacionUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCases/Evento/EventoModificacionUseCase.cs
@@ -1,16 +1,28 @@
 ﻿namespace CentroEventos.Aplicacion;
 
-public class EventoModificacionUseCase(IServicioAutorizacion auth,IRepositorioEventoDeportivo repoEven, EventoModificadorValidador validador) {
+public class EventoModificacionUseCase(IServicioAutorizacion auth,
+IRepositorioEventoDeportivo repoEven, IRepositorioReserva repoR, IRepositorioPersona repoPer,
+ EventoModificadorValidadorFecha vFec, EventoModificadorValidadorCupo vCupo,
+ EventoModificadorValidadorIdResponsable vId) {
 
-    public void Ejecutar(EventoDeportivo unEvento,int userId)
+    public void Ejecutar(EventoDeportivo unEvento, int userId)
     {
         if (auth.PoseeElPermiso(userId, Permiso.EventoModificacion))
         {
-            if (!validador.ValidarEvento(unEvento, repoEven, out string msg))
+            if (!vFec.ValidarEventoModificacionFecha(unEvento, repoEven, out string msg))
             {
-                throw new Exception(msg);
+                throw new ValidacionException(msg);
+            }
+            if (!vCupo.ValidarEventoModificacionCupoMax(unEvento, repoR, out string msg1))
+            {
+                throw new CupoExcedidoException(msg1);
+            }
+            if (!vId.ValidarEventoModificacionIdResponsable(unEvento, repoPer, out string msg2))
+            {
+                throw new EntidadNotFoundException(msg2);
             }
             repoEven.EventoModificacion(unEvento);
         }
+        else throw new FalloAutorizacionException("No se poseen permisos para realizar esta accion");
     }
 }
